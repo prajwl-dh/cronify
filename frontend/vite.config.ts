05feea553abfafa8e64f2801be1e705d0068856c
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-05-09 00:13:11 -0500

    Initialized the frontend with react, tailwind, vite, and vite-single-file plugin

---
+++ frontend/vite.config.ts
@@ -0,0 +1,17 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), viteSingleFile()],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true,
+      },
+    },
+  },
+});
