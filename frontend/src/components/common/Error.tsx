92e42683e2013e1eb80d652e79c3d53f56479b19
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-05-16 17:49:59 -0500

    Separated loading and error to separate reusable component

---
+++ frontend/src/components/common/Error.tsx
@@ -0,0 +1,22 @@
+export default function Error({ error }: { error: Error }) {
+  return (
+    <div className='fixed inset-0 flex items-center justify-center bg-(--background) text-(--primaryText)'>
+      <div className='text-center max-w-md'>
+        <h2 className='text-xl font-semibold mb-2'>Server unavailable</h2>
+        <p className='text-sm opacity-80 mb-4'>
+          Unable to connect to the backend. Make sure the cronify daemon is
+          running!
+        </p>
+
+        <p className='text-xs opacity-60 mb-4'>{error.message}</p>
+
+        <button
+          onClick={() => window.location.reload()}
+          className='px-4 py-2 bg-white text-black rounded-2xl hover:bg-gray-200 transition cursor-pointer select-none'
+        >
+          Reload
+        </button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/tasks/Tasks.tsx
+++ frontend/src/components/tasks/Tasks.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 import { APP_PORT } from '../../config/config';
+import Error from '../common/Error';
+import Loading from '../common/Loading';
 import FilterTask from './FilterTask';
 import TasksTable from './TasksTable';
 
@@ -15,47 +17,11 @@ export default function Tasks() {
   });
 
   if (isPending) {
-    return (
-      <div className='fixed inset-0 flex items-center justify-center bg-(--background) text-(--primaryText)'>
-        <div className='text-center'>
-          <div className='animate-spin h-10 w-10 border-4 border-(--primaryText) border-t-transparent rounded-full mx-auto mb-4' />
-          <p>Trying to connect to the server...</p>
-          <p className='text-sm opacity-80 pt-4'>
-            Make sure the cronify daemon is running!
-          </p>
-
-          <button
-            onClick={() => window.location.reload()}
-            className='mt-6 px-4 py-2 bg-white text-black rounded-2xl hover:bg-gray-200 transition shadow-md cursor-pointer select-none'
-          >
-            Reload
-          </button>
-        </div>
-      </div>
-    );
+    return <Loading />;
   }
 
   if (error) {
-    return (
-      <div className='fixed inset-0 flex items-center justify-center bg-(--background) text-(--primaryText)'>
-        <div className='text-center max-w-md'>
-          <h2 className='text-xl font-semibold mb-2'>Server unavailable</h2>
-          <p className='text-sm opacity-80 mb-4'>
-            Unable to connect to the backend. Make sure the cronify daemon is
-            running!
-          </p>
-
-          <p className='text-xs opacity-60 mb-4'>{error.message}</p>
-
-          <button
-            onClick={() => window.location.reload()}
-            className='px-4 py-2 bg-white text-black rounded-2xl hover:bg-gray-200 transition cursor-pointer select-none'
-          >
-            Reload
-          </button>
-        </div>
-      </div>
-    );
+    return <Error error={error} />;
   }
 
   return (
