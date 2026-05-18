2069e2baae77f4d79cf72fc84dbcf0542a132a10
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2026-05-18 13:18:52 -0500

    Now displaying cronify version on web ui as well

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -1,8 +1,27 @@
 import { Terminal } from 'lucide-react';
+import { useEffect, useState } from 'react';
+import { APP_PORT } from '../../config/config';
 import ActionButton from './ActionButton';
 import ThemePicker from './ThemePicker';
 
 export default function Navbar() {
+  const [appVersion, setAppVersion] = useState('');
+
+  useEffect(() => {
+    async function getAppVersion() {
+      try {
+        const res = await fetch(`http://localhost:${APP_PORT}/api/version`);
+        const data = (await res.json()) as { version: string };
+
+        setAppVersion(data.version);
+      } catch (error) {
+        console.error('Fetch app version error:', error);
+      }
+    }
+
+    getAppVersion();
+  }, []);
+
   return (
     <div className='w-full flex items-center justify-center bg-(--foreground) border-b border-(--border)'>
       <div className='w-full max-w-416 py-2 px-2 md:p-y4 md:px-6 flex justify-between items-center'>
@@ -11,9 +30,16 @@ export default function Navbar() {
           <div className='p-1 rounded-full bg-(--themeAccent)'>
             <Terminal className='w-3 h-3 md:w-5 md:h-5 text-white' />
           </div>
-          <span className='text-md md:text-xl font-black tracking-wider text-(--themeAccent)'>
-            CRONIFY
-          </span>
+          <div className='flex flex-col'>
+            <span className='text-md md:text-xl font-black tracking-wider text-(--themeAccent)'>
+              CRONIFY
+            </span>
+            {appVersion.length > 0 && (
+              <span className='text-xs text-(--secondaryText) tracking-wider -mt-1.5 ml-0.5'>
+                v{appVersion}
+              </span>
+            )}
+          </div>
         </div>
 
         {/* Theme picker and action button */}
